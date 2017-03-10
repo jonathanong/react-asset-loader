eaa1955f4271ca635636b5f7ea5763b8753ea49c
jongleberry <me@jongleberry.com>
2017-03-10 01:23:48 -0800

    refactor

--- src/__tests__/snapshots.js
+++ src/__tests__/snapshots.js
@@ -1,27 +1,45 @@
 
+import React, { Component, PropTypes } from 'react'
 import renderer from 'react-test-renderer'
 import assert from 'assert'
-import React from 'react'
 
-import AssetLoader from '..'
+import AssetLoader, { AssetMap } from '..'
 
 const src = 'https://js.stripe.com/v2/'
 
 it('should load the assets', async () => {
-  let component = renderer.create(
-    <AssetLoader urls={[src]}>
-      <div />
-    </AssetLoader>
-  )
+  class Child extends Component {
+    render () {
+      return (
+        <div className={this.props.className}>
+          <div>{JSON.stringify(this.props.assets)}</div>
+          <div>{String(this.props.assetsFulfilled)}</div>
+          <div>{String(this.props.assetsRejected)}</div>
+        </div>
+      )
+    }
+  }
+
+  Child.propTypes = {
+    className: PropTypes.string.isRequired,
+    assets: PropTypes.object.isRequired,
+    assetsFulfilled: PropTypes.bool.isRequired,
+    assetsRejected: PropTypes.bool.isRequired
+  }
+
+  const Thing = AssetLoader(Child, [
+    src
+  ])
+
+  let component = renderer.create(<Thing className='className' />)
 
   let tree = component.toJSON()
   expect(tree).toMatchSnapshot()
-  assert(tree.props.assets)
-  // assert(tree.props.assets[src])
-  // assert(!tree.props.assets[src].fulfilled)
-  // assert(!tree.props.assets[src].rejected)
-  assert(!tree.props.assetsFulfilled)
-  assert(!tree.props.assetsRejected)
 
-  await AssetLoader.AssetMap.get(src)
+  assert.equal('className', tree.props.className)
+
+  const promise = AssetMap.get(src)
+  assert(promise)
+  assert.equal('function', typeof promise.then)
+  await promise
 })
--- src/index.js
+++ src/index.js
@@ -1,71 +1,60 @@
 
-import { cloneElement, Children, Component, PropTypes } from 'react'
+import React from 'react'
 
 import { loadAsset } from './loaders'
 
 // global cache of assets loaded
-const AssetMap = new Map()
+export const AssetMap = new Map()
 
-export default class AssetLoader extends Component {
-  constructor (props) {
-    super(props)
+export default (Component, urls) => (
+  class AssetLoader extends Component {
+    constructor (props) {
+      super(props)
 
-    this.state = {
-      assets: {},
-      assetsFulfilled: false,
-      assetsRejected: false
-    }
-  }
-
-  componentDidMount () {
-    this.loadAssets(this.props.urls)
-  }
-
-  loadAssets (urls) {
-    const { assets } = this.state
-
-    const promises = urls.map(url => {
-      // already loaded
-      let promise = AssetMap.get(url)
-      if (!promise) {
-        promise = loadAsset(url)
-        AssetMap.set(url, promise)
+      this.state = {
+        assets: {},
+        assetsFulfilled: false,
+        assetsRejected: false
       }
-      return promise
-    })
-
-    this.setState({
-      assets
-    })
-
-    return Promise.all(promises).then(() => {
-      this.setState({
-        assetsFulfilled: true
-      })
-    }, () => {
-      this.setState({
-        assetsRejected: true
-      })
-    })
-  }
-
-  render () {
-    const { props, state } = this
-
-    if (Children.count(props.children) !== 1) {
-      throw new Error('Exactly 1 child is allowed.')
     }
 
-    const child = Children.only(props.children)
+    componentDidMount () {
+      this.loadAssets()
+    }
 
-    const passedProps = Object.assign({}, props, state)
-    delete passedProps.children
+    loadAssets () {
+      const { assets } = this.state
 
-    return cloneElement(child, passedProps)
+      const promises = urls.map(url => {
+        // already loaded
+        let promise = AssetMap.get(url)
+        if (!promise) {
+          promise = loadAsset(url)
+          AssetMap.set(url, promise)
+        }
+        return promise
+      })
+
+      this.setState({
+        assets
+      })
+
+      return Promise.all(promises).then(() => {
+        this.setState({
+          assetsFulfilled: true
+        })
+      }, () => {
+        this.setState({
+          assetsRejected: true
+        })
+      })
+    }
+
+    render () {
+      const props = Object.assign({}, this.props, this.state)
+      return (
+        <Component {...props} />
+      )
+    }
   }
-}
-
-AssetLoader.AssetMap = AssetMap
-AssetLoader.propTypes = {
-  urls: PropTypes.array.isRequired
-}
+)
